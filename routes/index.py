eb68a1f1011deb1cb38811be79a5dbf89dc6bfa7
harshitkaushik003 <harshitkaushik394@gmail.com>
2024-05-10 12:35:56 +0530

    added api routes

--- routes/index.py
+++ routes/index.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+import PyPDF2 as pdf
 
 api_blueprint = Blueprint('api', __name__);
 
@@ -9,3 +10,24 @@ def index():
     }
 
     return jsonify(data);
+
+
+@api_blueprint.route('/post', methods=['POST'])
+def post_trial():
+    data = request.json
+    return jsonify(data);
+
+@api_blueprint.route('/upload', methods=['POST'])
+def handle_upload():
+    file = request.files["file"]
+    pdf_reader = pdf.PdfReader(file)
+    length = len(pdf_reader.pages)
+    file_string = ''
+    for i in range(0, length):
+        page1 = pdf_reader.pages[i]
+        file_string += page1.extract_text()
+
+    data = {"fileContent": file_string}
+    return jsonify(data);
+
+
